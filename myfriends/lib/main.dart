ca3118376f63d796d9c8715c9c5f8c26488a4359
ITDevStar <itdevstar@hotmail.com>
2020-03-23 05:03:12 +0300

    navigation between views

--- myfriends/lib/main.dart
+++ myfriends/lib/main.dart
@@ -10,6 +10,9 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
         title: 'MyFriends',
+        theme: ThemeData (
+          primaryColor: Colors.white
+        ),
         home: RandomWords()
     );
   }
@@ -25,8 +28,43 @@ class RandomWordsState extends State<RandomWords> {
     return Scaffold(
       appBar: AppBar(
         title: Text('My Friends'),
+        actions: <Widget> [
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)
+        ]
       ),
       body: _buildSuggestions(),
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void> (
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map (
+              (WordPair pair) {
+                return ListTile (
+                  title: Text(
+                    pair.asPascalCase,
+                    style: _biggerFont,
+                  )
+                );
+              }
+          );
+          final List<Widget> divided = ListTile
+            .divideTiles(
+              context: context,
+              tiles: tiles
+            )
+            .toList();
+
+          return Scaffold (
+            appBar: AppBar(
+              title: Text('Saved Suggestion'),
+            ),
+            body: ListView(children: divided)
+          );
+        }
+      )
     );
   }
 
@@ -55,6 +93,15 @@ class RandomWordsState extends State<RandomWords> {
         alreadySaved ? Icons.favorite : Icons.favorite_border,
         color: alreadySaved ? Colors.red : null,
       ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 }
