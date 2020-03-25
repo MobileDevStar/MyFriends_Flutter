902ae78235eed0ed2c447193b4560e5f94111cc6
ITDevStar <itdevstar@hotmail.com>
2020-03-25 18:09:12 +0300

    detailed screen

--- myfriends/lib/main.dart
+++ myfriends/lib/main.dart
@@ -7,14 +7,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
         title: 'MyFriends',
-        theme: ThemeData (
-          primaryColor: Colors.white
-        ),
-        home: RandomWords()
-    );
+        //theme: ThemeData(primaryColor: Colors.white),
+        home: RandomWords());
   }
 }
 
@@ -26,45 +22,147 @@ class RandomWordsState extends State<RandomWords> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('My Friends'),
-        actions: <Widget> [
-          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)
-        ]
-      ),
+      appBar: AppBar(title: Text('My Friends'), actions: <Widget>[
+        IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)
+      ]),
       body: _buildSuggestions(),
     );
   }
 
   void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void> (
-        builder: (BuildContext context) {
-          final Iterable<ListTile> tiles = _saved.map (
-              (WordPair pair) {
-                return ListTile (
-                  title: Text(
-                    pair.asPascalCase,
-                    style: _biggerFont,
-                  )
-                );
-              }
-          );
-          final List<Widget> divided = ListTile
-            .divideTiles(
-              context: context,
-              tiles: tiles
-            )
-            .toList();
+    Navigator.of(context)
+        .push(MaterialPageRoute<void>(builder: (BuildContext context) {
+      final Iterable<ListTile> tiles = _saved.map((WordPair pair) {
+        return ListTile(
+            title: Text(
+            pair.asPascalCase,
+            style: _biggerFont,
+          ),
+          trailing: Icon(
+            Icons.favorite,
+            color: Colors.red ,
+          ),
+          onTap: () {
+            _pushDetails();
+        });
+      });
+      final List<Widget> divided =
+          ListTile.divideTiles(context: context, tiles: tiles).toList();
 
-          return Scaffold (
-            appBar: AppBar(
-              title: Text('Saved Suggestion'),
+      return Scaffold(
+          appBar: AppBar(
+            title: Text('Saved Suggestion'),
+          ),
+          body: ListView(children: divided));
+    }));
+  }
+
+  void _pushDetails() {
+    Navigator.of(context)
+        .push(MaterialPageRoute<void>(builder: (BuildContext context) {
+
+      Widget titleSection = Container(
+        padding: const EdgeInsets.all(32),
+        child: Row(
+          children: [
+            Expanded(
+              /*1*/
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  /*2*/
+                  Container(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: Text(
+                      'Oeschinen Lake Campground',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    'Kandersteg, Switzerland',
+                    style: TextStyle(
+                      color: Colors.grey[500],
+                    ),
+                  ),
+                ],
+              ),
             ),
-            body: ListView(children: divided)
-          );
-        }
-      )
+            /*3*/
+            Icon(
+              Icons.star,
+              color: Colors.red[500],
+            ),
+            Text('41'),
+          ],
+        ),
+      );
+
+      Color color = Theme.of(context).primaryColor;
+
+      Widget buttonSection = Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            _buildButtonColumn(color, Icons.call, 'CALL'),
+            _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+            _buildButtonColumn(color, Icons.share, 'SHARE'),
+          ],
+        ),
+      );
+
+      Widget textSection = Container(
+        padding: const EdgeInsets.all(32),
+        child: Text(
+          'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+              'Alps. Situated 1,578 meters above sea level, it is one of the '
+              'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+              'half-hour walk through pastures and pine forest, leads you to the '
+              'lake, which warms to 20 degrees Celsius in the summer. Activities '
+              'enjoyed here include rowing, and riding the summer toboggan run.',
+          softWrap: true,
+        ),
+      );
+
+      return Scaffold(
+          appBar: AppBar(
+            title: Text('Saved Suggestion'),
+          ),
+          body: ListView(
+              children: [
+                Image.asset(
+                  'images/lake.jpg',
+                  width: 600,
+                  height: 240,
+                  fit: BoxFit.cover,
+                ),
+                titleSection,
+                buttonSection,
+                textSection
+              ],
+          ));
+    }));
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 
